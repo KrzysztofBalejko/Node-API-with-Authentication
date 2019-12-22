91bac1331599d7faf34713c1bfa2091eabd0469a
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-22 19:37:06 +0000

    :package: adding dotenv to hide username and password

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// Connnect to db
+dotenv.config();
+
+mongoose.connect(
+    process.env.DB_CONNECT,
+    options,
+    () => console.log('connected to DB !')
+);
 
 // Import routes
 const authRoute = require('./routes/auth');
