a9f57c37dc61694f2f075192d538bdb2f2b76327
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-23 17:10:06 +0000

    :sparkles: login validation

--- validation.js
+++ validation.js
@@ -17,6 +17,20 @@ const registerValidation = (data) => {
     return Joi.ValidationError(data, schema);
 }
 
+// Login validation
+const loginValidation = (data) => {
+    const schema = {
+        email: Joi.string()
+                  .min(6)
+                  .required()
+                  .email(),
+        password: Joi.string()
+                     .min(6)
+                     .required()
+    }
+    return Joi.ValidationError(data, schema);
+}
+
 module.exports.registerValdiation = registerValidation;
 
 
