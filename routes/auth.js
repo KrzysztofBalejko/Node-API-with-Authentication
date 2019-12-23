7968e2f56999e38ad4935c490097e4271da91a00
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-23 16:52:53 +0000

    :sparkles: working authentication

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,18 @@ const router = express.Router();
 // Validation
 const Joi = require('@hapi/joi');
 
+const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+}
+
 
 router.post('/register', async (req,res) => {
+
+    // Validating data before user
+    const {error} = Joi.validate(req.body, schema);
+    res.send(error.details[0].message);
 
     const user = new User({
         name: req.body.name,
