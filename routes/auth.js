f7b40202480a77ccd636f3881fca2998f3d3a45a
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-24 13:07:01 +0000

    :sparkles: checks if user is in db

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,17 @@ const { registerValidation } = require('../validation');
 
 router.post('/register', async (req,res) => {
 
+    // Validating data
     const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    // Checking if the user is already in the db
+    const emailExist = await User.findOne({
+        email: req.body.email 
+    });
+    if(emailExist) return res.status(400).send('Email already exists');
+
+    // Creating new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
