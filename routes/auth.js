2465a915c39e5f615c7ce6e0b37fca17ee199c36
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-12-24 13:30:12 +0000

    :sparkles:  logging in

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,13 @@ const express = require('express');
 const User = require('../model/User');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValdiation } = require('../validation');
 
 
 router.post('/register', async (req,res) => {
 
     // Validating data
-    const {error} = registerValidation(req.body);
+    const { error } = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
     // Checking if the user is already in the db
@@ -34,6 +34,25 @@ router.post('/register', async (req,res) => {
     }catch(err){
         res.status(400).send(err);
     }
+});
+
+router.post('/login', async (req, res) => {
+
+    // Validating data
+    const { error } = loginValdiation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    // Checking if the user is already in the db
+    const user = await User.findOne({
+        email: req.body.email 
+    });
+    if(!user) return res.status(400).send('Email not found');
+    
+    // Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid password');
+    res.send('Logged in');
+
 });
 
 
